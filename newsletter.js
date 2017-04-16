94998682c7fbd1f902757ccc2edb7370b0872dd7
zacboz <zacboz@gmail.com>
2017-04-16 01:39:45 -0600

    finished newsletter signup popup

--- newsletter.js
+++ newsletter.js
@@ -1,6 +1,28 @@
-// $(document).scroll(function() {
-//   var y = $(this).scrollTop();
-//   if (y > 800) {
-//   } else {
-//   }
-// });
+$(document).ready(function(){
+  // $('#newsletter').removeClass('show-newsletter-nav');
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 100) {
+      $('#newsletter').addClass('show-newsletter-nav');
+    }
+  });
+
+  $('.close-newsletter').on('click', function() {
+    $('#newsletter').addClass('hide-newsletter-nav');
+  });
+
+  $('.open-newsletter').on('click', function() {
+    $('.newsletter-wrapper').removeClass('hide-newsletter-popup');
+    $('#newsletter').addClass('hide-newsletter-nav');
+  });
+
+  $('.newsletter-popup-close').on('click', function() {
+    $('.newsletter-wrapper').addClass('hide-newsletter-popup');
+  });
+
+  $('.sign-up-success').on('click', function() {
+    $('.newsletter-wrapper').addClass('hide-newsletter-popup');
+  });
+
+});
